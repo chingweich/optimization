690235c0f617fb220efc4b3a34647c60d361245c
chingweich <cat8007@gmail.com>
2015-08-05 22:24:53 +0200

    makeEff add another def. of sign.

--- makeEff.C
+++ makeEff.C
@@ -17,6 +17,8 @@
 #include "TStyle.h"
 #include "untuplizer.h"
 #include <string>
+#include "TGaxis.h"
+#include "TPad.h"
 
 #define  nWidth 12
 #define  nBmin 41 
@@ -57,8 +59,9 @@ void makeEff(){
   c1 = new TCanvas("c1","",1360,768);
   string  masspoint[13]={"600","800","1000","1200","1400","1600","1800","2000","2500","3000","3500","4000","4500"};
   for (int massP=0;massP<13;massP++){
-  TString fin =Form("root_files/signal-%s.root",masspoint[massP].data())
-  ,fin2 = Form("root_files/BulkGravitonZlepZqq-%s.root",masspoint[massP].data());
+  TString fin =Form("root_files/signal-%s.root",masspoint[massP].data()),
+  //fin2 = Form("root_files/BulkGravitonZlepZqq-%s.root",masspoint[massP].data());
+  fin2="root_files/DYBkg.root";
   f= TFile::Open(fin.Data());
   f2= TFile::Open(fin2.Data());
   TH1F * th1 = (TH1F*)f->FindObjectAny("HMass");
@@ -67,6 +70,7 @@ void makeEff(){
   int bmin[nBmin]={81,82,83,84,85,86,87,88,89,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120};
   double eff[nWidth][nBmin],eff2[nWidth][nBmin],err[nWidth][nBmin],err2[nWidth][nBmin];
   double sign[nWidth][nBmin],signCP[nWidth][nBmin],signErr[nWidth][nBmin];
+  double signNum[nWidth][nBmin],signNumCP[nWidth][nBmin],signNumErr[nWidth][nBmin];
   for(int i=0;i<nWidth;i++){
     for(int j=0;j<nBmin;j++){
       eff[i][j]=getEff(th1,bmin[j],bmin[j]+width[i]);
@@ -74,12 +78,17 @@ void makeEff(){
 	  err[i][j]=getErr(th1,bmin[j],bmin[j]+width[i]);
       err2[i][j]=getErr(th2,bmin[j],bmin[j]+width[i]);
       sign[i][j]=getSign(eff[i][j],eff2[i][j]);
-      signCP[i][j]=getSign(eff[i][j],eff2[i][j]);
+	  signNum[i][j]=getSign(eff[i][j],eff2[i][j]*th2->Integral());
+      signCP[i][j]=sign[i][j];
+      signNumCP[i][j]=signNum[i][j];
 	  signErr[i][j]=(1/(1+sqrt(eff2[i][j])))*sqrt(err[i][j]*err[i][j]+ (eff[i][j]*eff[i][j]*err2[i][j]*err2[i][j])/(4*eff2[i][j]*(1+sqrt(eff2[i][j]))*(1+sqrt(eff2[i][j]))));
-      //cout<<"range={"<<bmin[j]<<","<<bmin[j]+width[i]<<"} and efficiency="<<eff[i][j]<<endl;
+      signNumErr[i][j]=(1/(1+sqrt(eff2[i][j]*th2->Integral())))*sqrt(err[i][j]*err[i][j]+ (eff[i][j]*eff[i][j]*err2[i][j]*err2[i][j]*th2->Integral())/(4*eff2[i][j]*(1+sqrt(eff2[i][j]*th2->Integral()))*(1+sqrt(eff2[i][j]*th2->Integral()))));
+
+	  //cout<<"range={"<<bmin[j]<<","<<bmin[j]+width[i]<<"} and efficiency="<<eff[i][j]<<endl;
       //cout<<"range=["<<bmin[j]<<","<<bmin[j]+width[i]<<"] and significant="<<sign[i][j]<<endl;
     }
   }
+  
   vector<int> signI,signJ;
   for(int ij=0;ij<nWidth*nBmin;ij++){
     double tempSign=0;
@@ -98,6 +107,26 @@ void makeEff(){
     signJ.push_back(tempJ);
     //cout<<signCP[tempI][tempJ]<<endl;
     signCP[tempI][tempJ]=0;
+  }
+  
+  vector<int> signINum,signJNum;
+  for(int ij=0;ij<nWidth*nBmin;ij++){
+    double tempSign=0;
+    int tempI=0,tempJ=0;
+    for(int i=0;i<nWidth;i++){
+      for(int j=0;j<nBmin;j++){
+        if(signNumCP[i][j]>tempSign){
+          //cout<<"i="<<i<<",j="<<j<<",signcp="<<signCP[i][j]<<",temp="<<tempSign<<endl;
+          tempSign=signNumCP[i][j];
+	  tempI=i;
+	  tempJ=j;
+	}
+      }
+    }
+    signINum.push_back(tempI);
+    signJNum.push_back(tempJ);
+    //cout<<signCP[tempI][tempJ]<<endl;
+    signNumCP[tempI][tempJ]=0;
   }
   
   int numberBin=15;
@@ -140,6 +169,7 @@ void makeEff(){
   th1Sign->SetTitle(Form("largest 15 Sign. windows,%sGev",masspoint[massP].data()));
   th1Sign->SetMinimum(0);
   th1Sign->SetMaximum(1);
+  th1Sign->SetLineColor(4);
   th1Sign->Draw();
   th1Eff->SetLineColor(2);
   th1Eff2->SetLineColor(3);
@@ -160,12 +190,96 @@ void makeEff(){
   th1SignFull->SetTitle(Form("%s",masspoint[massP].data()));
   th1SignFull->SetMinimum(0);
   th1SignFull->SetMaximum(1);
+  th1SignFull->SetLineColor(4);
   th1SignFull->Draw("Hist");
   th1EffFull->SetLineColor(2);
   th1Eff2Full->SetLineColor(3);
   th1EffFull->Draw("same");
   th1Eff2Full->Draw("same");
   leg->Draw("same");
+  
+  c1->Print("pdf/signv2.pdf");
+  
+  for(int i=0;i<numberBin;i++){
+	  th1Sign->SetBinContent(i+1,signNum[signINum[i]][signJNum[i]]);
+	  th1Sign->SetBinError(i+1,signNumErr[signINum[i]][signJNum[i]]);
+	  th1Eff->SetBinContent(i+1,eff[signINum[i]][signJNum[i]]);
+	  th1Eff2->SetBinContent(i+1,eff2[signINum[i]][signJNum[i]]);
+	  th1Eff->SetBinError(i+1,err[signINum[i]][signJNum[i]]);
+	  th1Eff2->SetBinError(i+1,err2[signINum[i]][signJNum[i]]);
+      th1Eff->GetXaxis()->SetBinLabel(i+1,Form("%d-%d",bmin[signJNum[i]],bmin[signJNum[i]]+width[signINum[i]]));
+    //cout<<"range=["<<bmin[signJ[i]]<<","<<bmin[signJ[i]]+width[signI[i]]<<"] and significant="<<sign[signI[i]][signJ[i]]<<endl;
+  }
+  
+  
+  for(unsigned int i=0;i<signINum.size();i++){
+	  th1SignFull->SetBinContent(i+1,signNum[signINum[i]][signJNum[i]]);
+	  th1SignFull->SetBinError(i+1,signNumErr[signINum[i]][signJNum[i]]);
+	  th1EffFull->SetBinContent(i+1,eff[signINum[i]][signJNum[i]]);
+	  th1Eff2Full->SetBinContent(i+1,eff2[signINum[i]][signJNum[i]]);
+	  th1EffFull->SetBinError(i+1,err[signINum[i]][signJNum[i]]);
+	  th1Eff2Full->SetBinError(i+1,err2[signINum[i]][signJNum[i]]);
+      //th1SignFull->GetXaxis()->SetBinLabel(i+1,Form("%d-%d",bmin[signJ[i]],bmin[signJ[i]]+width[signI[i]]));
+      th1SignFull->GetXaxis()->SetBinLabel(i+1,"");
+    //cout<<"range=["<<bmin[signJ[i]]<<","<<bmin[signJ[i]]+width[signI[i]]<<"] and significant="<<sign[signI[i]][signJ[i]]<<endl;
+  }
+  
+  th1Eff->SetMinimum(0);
+  th1Eff->SetMaximum(1);
+  th1EffFull->SetMinimum(0);
+  th1EffFull->SetMaximum(1);
+  th1Eff->SetYTitle("efficiency");
+  th1EffFull->SetYTitle("efficiency");
+  
+  th1Eff->Draw();
+  th1Eff2->Draw("same");
+  
+  Float_t rightmax = 2*th1Sign->GetBinContent(1);
+  //cout<<rightmax<<endl;
+  Float_t scale = gPad->GetUymax()/rightmax;
+  //hint1->SetLineColor(kRed);
+  th1Sign->Scale(scale);
+  //hint1->Draw("same");
+  //draw an axis on the right side
+  c1->Update();
+  th1Sign->Draw("same");
+  //leg->Clear();
+  //leg->AddEntry(th1Eff,"signal efficiency");
+  //leg->AddEntry(th1Eff2,"Bkg. efficiency");
+  //leg->SetY1(0.8335);
+  leg->Draw("same");
+  
+  TGaxis *axis = new TGaxis(gPad->GetUxmax(),gPad->GetUymin(),
+         gPad->GetUxmax(), gPad->GetUymax(),0,rightmax,510,"+L");
+   axis->SetTitle("significance");
+   axis->SetTitleColor(4);
+   axis->SetLineColor(4);
+   axis->SetLabelColor(4);
+   axis->Draw();
+  
+  c1->Print("pdf/signv2.pdf");
+  
+  
+  
+  
+  
+  th1EffFull->Draw();
+  th1Eff2Full->Draw("same");
+  c1->Update();
+  rightmax = 1.1*th1SignFull->GetBinContent(1);
+  scale = gPad->GetUymax()/rightmax;
+  th1SignFull->Scale(scale);
+  
+  th1SignFull->Draw("Hist,same");
+  leg->Draw("same");
+  
+  TGaxis *axis2 = new TGaxis(gPad->GetUxmax(),gPad->GetUymin(),
+         gPad->GetUxmax(), gPad->GetUymax(),0,rightmax,510,"+L");
+   axis2->SetTitle("significance");
+   axis2->SetTitleColor(4);
+   axis2->SetLineColor(4);
+   axis2->SetLabelColor(4);
+   axis2->Draw();
   
   if(massP==12)c1->Print("pdf/signv2.pdf)");
   else c1->Print("pdf/signv2.pdf");
